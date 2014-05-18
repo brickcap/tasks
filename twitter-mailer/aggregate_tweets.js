fe2f791d1e73e1e556fa76c3b0c9a372ab9c8ef1
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2014-05-18 18:07:05 +0530

    Got the tweets from the list

---
+++ twitter-mailer/aggregate_tweets.js
@@ -0,0 +1,16 @@
+var twit = require("twit");
+
+var twitter = new twit({
+    consumer_key:         'ucGZ3YXP7H6zvSUhl2C92u4CO',
+    consumer_secret:      'x3kaxss7QcfU2AaJT9aU2G94ks8s3RJhpBbdu09FhU3HOzTWI5',
+    access_token:         '580679352-uMMRMxjep9gxBLmsWQDlHqP8ozXel58aFPlfgv8W',
+    access_token_secret:  'tzDaEuXmb6lF3wATKJdbNnjMM3UC1JBWZrgz1Ly36YIdm'
+});
+
+twitter.get("/lists/statuses",
+	    {slug:"mailer",owner_screen_name:"brickcap",per_page:50},
+	    function(error,data,response){
+		console.log(error);
+		console.log(data);
+		return;
+	    });
