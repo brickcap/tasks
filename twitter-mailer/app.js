1ed30f62f7d3e1f9009d5f088e75a56b86c6fdee
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2014-05-18 15:17:32 +0530

    Mustache view engine set up

--- twitter-mailer/app.js
+++ twitter-mailer/app.js
@@ -4,15 +4,17 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var consolidate =require("consolidate");
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
+app.engine("html",consolidate.mustache);
+app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
 
 app.use(favicon());
 app.use(logger('dev'));
