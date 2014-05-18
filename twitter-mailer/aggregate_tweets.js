c2b6d878560153163c11a027ac83fd280f4bcacf
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2014-05-18 22:03:37 +0530

    Created a basic template

--- twitter-mailer/aggregate_tweets.js
+++ twitter-mailer/aggregate_tweets.js
@@ -1,23 +1,32 @@
 var twit = require("twit");
-var _ = require("_");
-
-var twitter = new twit({
-    consumer_key:         'ucGZ3YXP7H6zvSUhl2C92u4CO',
-    consumer_secret:      'x3kaxss7QcfU2AaJT9aU2G94ks8s3RJhpBbdu09FhU3HOzTWI5',
-    access_token:         '580679352-uMMRMxjep9gxBLmsWQDlHqP8ozXel58aFPlfgv8W',
-    access_token_secret:  'tzDaEuXmb6lF3wATKJdbNnjMM3UC1JBWZrgz1Ly36YIdm'
-});
+var _ = require("underscore");
+var agg = {};
+var state = ["KaseyaCorp","@hosted_kaseya","@kaseya_backup","KaseyaUK","sundar_tweets" ];
+var twitter = new twit(
+    {
+	consumer_key:         'ucGZ3YXP7H6zvSUhl2C92u4CO',
+	consumer_secret:      'x3kaxss7QcfU2AaJT9aU2G94ks8s3RJhpBbdu09FhU3HOzTWI5',
+	access_token:         '580679352-uMMRMxjep9gxBLmsWQDlHqP8ozXel58aFPlfgv8W',
+	access_token_secret:  'tzDaEuXmb6lF3wATKJdbNnjMM3UC1JBWZrgz1Ly36YIdm'
+    });
 
 exports.agg = function(res,wss){
-    twitter.get("/lists/statuses",
-		{slug:"mailer",owner_screen_name:"brickcap",per_page:50},
-		function(error,data,response){
-		    console.log(error);
-		    console.log(data);
-		    if(res)return res.send(data);
-		    if(wss){
-		    };
-		    return null;
-		});
-    
+    var name = state.pop();
 };
+
+function twitter_request(name,res,wss){
+    twitter.get("statuses/user_timeline",
+		{screen_name:"KaseyaCorp",count:10},
+		function(error,data,response){
+		    //check if the tweet for all the users have been aggregated
+		    var state_length = state.length===0;
+		    //get all the tweets from the unstructured data
+		    if(data) agg.data.name = _.pluck(data,"text");
+		    
+		    if(state_length)return res.send(data);
+		    if(state_length)return res.send(data);
+		    twitter_request(state.pop(),res,wss);
+		    return agg;
+		});
+
+}
--- twitter-mailer/routes/index.js
+++ twitter-mailer/routes/index.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var agg = require("../aggregate-tweets");
+var tweet = require("../aggregate_tweets");
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    return r
-  res.render('index', { title: 'Express' });
+    return res.render("index");
 });
 
 module.exports = router;
