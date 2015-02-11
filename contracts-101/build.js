586947d83182457710d70a20eb6f74b3da06f626
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2015-02-11 10:14:29 +0530

    Added the build script

---
+++ contracts-101/build.js
@@ -0,0 +1,33 @@
+var buildify = require("buildify");
+var build_path = "/home/akshat/Repo/wrinqPreview/wrinq-front-end/blog/";
+var fs = require("fs");
+var marked = require("marked");
+var mustache = require("mustache");
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false
+});
+
+buildify.task({
+    name:"convert-book",
+    task:function(){
+	console.log("starting book conversion....");
+	var book_template = fs.readFileSync(build_path+"book_template.html");
+	var book = fs.readFileSync("./book.md");
+	var html = marked(book.toString());
+	var rendered = mustache.render(book_template.toString(),{html:html});
+	fs.writeFileSync(build_path+"index.html",rendered);
+	console.log("Book has been built");
+    }
+
+});
+
+
+
